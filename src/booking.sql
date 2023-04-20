3bab4a04965b14d9b39016d77f133989760ab02f
noah-fullerton <noahmfullerton@gmail.com>
2023-04-20 11:28:02 -0600

    ReservationsView implemented

--- src/booking.sql
+++ src/booking.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student(s): 
+-- Student(s): Noah Fullerton, Muhammad Qamar
 -- Description: booking database
 
 DROP DATABASE booking;
@@ -86,6 +86,13 @@ GRANT ALL ON SEQUENCE users_user_seq TO booking;
 --     3 | 2023-06-23 | E      | 14:00:00 | 16:00:00 | AES-220 | Morbid Mojito
 --     2 | 2023-06-12 | D      | 12:00:00 | 14:00:00 | AES-210 | Sam Mai Tai
 --     1 | 2023-05-15 | C      | 10:00:00 | 12:00:00 | AES-210 | Sam Mai Tai
+
+CREATE VIEW ReservationsView AS 
+SELECT code, date, period, C.start, C."end", concat_ws('-', abbr, room) as room, B.name 
+FROM Reservations A
+NATURAL JOIN Users B
+NATURAL JOIN Periods C
+ORDER BY code DESC;
 
 -- once your view is created and working, run the following line
 GRANT ALL ON TABLE ReservationsView TO booking;
